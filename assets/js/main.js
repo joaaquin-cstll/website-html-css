36d1f192049a69f82f447376c28bce4e945986d2
joaquin <joaaquin.cstll@gmail.com>
2025-12-09 15:49:33 +0100

    update5

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,9 +38,45 @@ window.addEventListener('scroll', blurHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '3000',
+    delay: '400',
+    // reset: true //Animations Repeat
+})
+
+sr.reveal('.home__data, .explore__data, .explore__user, .footer__container')
+sr.reveal('.home__card', {delay: 600, distance: '100px', interval:100})
+sr.reveal('.about__data, .join__image', {origin: 'right'})
+sr.reveal('.about__image, .join__data', {origin: 'left'})
+sr.reveal('.popular__card', {intercal: 200})
